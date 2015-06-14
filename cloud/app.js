14f7b6dcaf9776df2d6d93b3a3e91c0d730da91b
Devin Lumley <keiroshin@gmail.com>
2015-06-14 11:16:56 -0700

    Adding cloud job to apply balances at user's 12am time, and cloud function for sending reminders

--- cloud/app.js
+++ cloud/app.js
@@ -647,8 +647,17 @@ Parse.Cloud.job("DailyBalanceUsersTime", function(request, status) {
                 var dailyBudget = user.get('dailyBudget');
                 user.increment('todaysBudget', dailyBudget * diffDays);
                 user.set('lastDailyBudgetUpdate', today.utc().toDate());
+
+                Parse.Cloud.run('SendBalanceReminder', { userId: user.id }, {
+                    success: function(result) {},
+                    error: function(error) {}
+                });
+
+                return user.save();
             }
-            return user.save();
+            else {
+                return false;
+            }
         }
         else {
             return false;
@@ -805,6 +814,160 @@ Parse.Cloud.job("DailyBalanceReminder", function(request, status) {
         status.success("DailyBalanceReminder ran successfully.");
     }, function(error) {
         status.error("Error while running DailyBalanceReminder:" + error);
+    });
+});
+
+Parse.Cloud.define("SendBalanceReminder", function(request, response) {
+    var currencies = [
+        {
+          'currency': 'usd',
+          'character': '$',
+          'character_unicode': "\u0024",
+          'placeholder': '00.00',
+          'decimalPlaces': 2,
+          'centsToWhole': 100,
+          'icon': 'fa-usd',
+          'format': 'left',
+          'sliderUnit': 1
+        },
+        {
+          'currency': 'gbp',
+          'character': '£',
+          'character_unicode': "\u00A3",
+          'placeholder': '00.00',
+          'decimalPlaces': 2,
+          'centsToWhole': 100,
+          'icon': 'fa-gbp',
+          'format': 'left',
+          'sliderUnit': 1
+        },
+        {
+          'currency': 'euro',
+          'character': '€',
+          'character_unicode': "\u20AC",
+          'placeholder': '00.00',
+          'decimalPlaces': 2,
+          'centsToWhole': 100,
+          'icon': 'fa-euro',
+          'format': 'left',
+          'sliderUnit': 1
+        },
+        {
+          'currency': 'yen',
+          'character': '¥',
+          'character_unicode': "\u00A5",
+          'placeholder': '00.00',
+          'decimalPlaces': 2,
+          'centsToWhole': 100,
+          'icon': 'fa-yen',
+          'format': 'left',
+          'sliderUnit': 1
+        },
+        {
+          'currency': 'lira',
+          'character': '₺',
+          'character_unicode': "\u20BA",
+          'placeholder': '00.00',
+          'decimalPlaces': 2,
+          'centsToWhole': 100,
+          'icon': 'fa-try',
+          'format': 'left',
+          'sliderUnit': 1
+        }
+    ];
+
+    function getCurrency(key) {
+        for(var i in currencies) {
+          var currency = currencies[i];
+          if(currency.currency == key) {
+            return currency;
+          }
+        }
+
+        return currencies[0];
+    }
+
+    function toWhole(amountInCents, currency) {
+        if(typeof currency === 'undefined') currency = currencies[0];
+        var divisor = currency.centsToWhole;
+        return parseFloat(amountInCents / divisor).toFixed(currency.decimalPlaces);
+    }
+
+    function toDisplay(amountInCents, currency) {
+        amountInCents = Math.round(amountInCents);
+        currentCurrency = currency;
+        if(currentCurrency) {
+          var negativeSymbol = "";
+          if(amountInCents < 0) {
+            negativeSymbol = "-";
+            amountInCents = Math.abs(amountInCents);
+          }
+          return negativeSymbol + currentCurrency.character_unicode + toWhole(amountInCents, currentCurrency);
+        }
+    }
+    
+    Parse.Cloud.useMasterKey();
+    var query = new Parse.Query(Parse.User);
+    var userId = request.params.userId;
+
+    if(!userId) {
+        response.error('No userid present');
+    }
+
+    query.equalTo('objectId', userId);
+    query.limit(1);
+    query.first({
+        success: function(user) {
+            var deviceToken = user.get('deviceToken');
+
+            if(deviceToken && user.get('allowanceReminders') !== false) {
+                var currency = getCurrency(user.get('currency'));
+                var tBWhole = toDisplay(user.get('todaysBudget'), currency);
+
+                var msg = "Bwraaak!  Your allowance today is " + tBWhole + "!";
+                var noteBody = {
+                  "tokens":[
+                    deviceToken
+                  ],
+                  "notification":{
+                    "alert": msg,
+                    "ios":{
+                        "sound": "www/sounds/spend.wav",
+                        "contentAvailable": true,
+                    },
+                    "android":{
+                      "delayWhileIdle":true,
+                    }
+                  }
+                }
+
+                var buf = new Buffer('07e9ea0648e6a6f507e91a2914237509230499c70c4eaf9f', 'utf8');
+                var base64 = buf.toString('base64');
+
+                Parse.Cloud.httpRequest({
+                    url: 'https://push.ionic.io/api/v1/push',
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json;charset=utf-8',
+                        'X-Ionic-Application-Id': 'b306568f',
+                        'Authorization': 'Basic ' + base64 + ':'
+                    },
+                    body: JSON.stringify(noteBody),
+                    success: function(httpResponse) {
+                        response.success('Notification sent');
+                    },
+                    error: function(httpResponseError) {
+                        response.error(httpResponseError);
+                    }
+                });
+            }
+            else {
+                response.error('No device token present or notifications disabled by user');
+            }
+        },
+        error: function(error) {
+            response.error(error);
+        }
     });
 });
 
